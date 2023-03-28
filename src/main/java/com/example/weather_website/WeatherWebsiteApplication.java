15cdeb8453e3409f0c076988d12e041c2cf5e3f9
Dominastorm <dchawla228@gmail.com>
2023-03-28 15:56:59 +0530

    first commit

---
+++ src/main/java/com/example/weather_website/WeatherWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.weather_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherWebsiteApplication.class, args);
+	}
+
+}
