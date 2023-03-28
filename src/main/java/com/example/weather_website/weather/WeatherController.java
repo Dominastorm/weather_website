165a28f0e1b47f52c8e9f61e3bd88aee578340e0
Dominastorm <dchawla228@gmail.com>
2023-03-28 18:38:59 +0530

    Add Autowired bean

--- src/main/java/com/example/weather_website/weather/WeatherController.java
+++ src/main/java/com/example/weather_website/weather/WeatherController.java
@@ -2,6 +2,7 @@ package com.example.weather_website.weather;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ public class WeatherController {
     
 	private final WeatherService weatherService;
 
+	@Autowired
 	public WeatherController(WeatherService weatherService) {
 		this.weatherService = weatherService;
 	} 
