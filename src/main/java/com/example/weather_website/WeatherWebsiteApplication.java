917d2506d688a6c52e21c3bf8610da9b6bb477b6
Dominastorm <dchawla228@gmail.com>
2023-03-28 18:04:49 +0530

    Move request to WeatherController

--- src/main/java/com/example/weather_website/WeatherWebsiteApplication.java
+++ src/main/java/com/example/weather_website/WeatherWebsiteApplication.java
@@ -1,38 +1,13 @@
 package com.example.weather_website;
 
-import java.time.LocalDate;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.weather_website.weather.Weather;
 
 @SpringBootApplication
-@RestController
 public class WeatherWebsiteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherWebsiteApplication.class, args);
 	}
 
-	@GetMapping("/")
-	public List<Weather> hello() {
-		return List.of(
-			new Weather(
-				"London",
-				LocalDate.now(),
-				10.0,
-				80.0
-			),		
-			new Weather(
-				"Paris",
-				LocalDate.now(),
-				12.0,
-				70.0
-			)
-		);
-	}
 }
