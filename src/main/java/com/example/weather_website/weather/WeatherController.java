6ac2fc53f1b73aa72e30b43f3b213d207d414485
Dominastorm <dchawla228@gmail.com>
2023-03-29 20:52:58 +0530

    Display get_weather on a web page

--- src/main/java/com/example/weather_website/weather/WeatherController.java
+++ src/main/java/com/example/weather_website/weather/WeatherController.java
@@ -3,32 +3,28 @@ package com.example.weather_website.weather;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
-@RequestMapping(path = "/api/v1/weather")
+@Controller
 public class WeatherController {
-    
-	@GetMapping
-	public List<Weather> hello() {
-		return List.of(
-			new Weather(
-                1L,
-				"London",
-				LocalDate.now(),
-				10.0,
-				80.0
-			),		
-			new Weather(
-                2L,
-				"Paris",
-				LocalDate.now(),
-				12.0,
-				70.0
-			)
-		);
+	@GetMapping("/get_weather")
+	public String getWeather(@RequestParam(name = "weathers", required = false) List<Weather> weathers, Model model) {
+		model.addAttribute("weathers", List.of(
+				new Weather(
+						1L,
+						"London",
+						LocalDate.now(),
+						10.0,
+						80.0),
+				new Weather(
+						2L,
+						"Paris",
+						LocalDate.now(),
+						12.0,
+						70.0)));
+		return "getWeather";
 	}
-    
 }
